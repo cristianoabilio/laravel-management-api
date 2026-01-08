888ed286604a228c5bb9e10a2f10c30641b0095e
Cristiano Abilio <cristianocafr@gmail.com>
2026-01-08 15:29:29 -0300

    feat: updated to use factory and status from enum class

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,10 +2,18 @@
 
 namespace App\Models;
 
+use App\TaskStatus;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
+    use HasFactory;
+
+    protected $casts = [
+        'status' => TaskStatus::class,
+    ];
+
     protected $fillable = [
         'title',
         'project_id',
