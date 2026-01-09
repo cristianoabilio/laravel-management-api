9e91662da9a7d5492a35536efa329155e4c750f2
Cristiano Abilio <cristianocafr@gmail.com>
2026-01-09 12:32:10 -0300

    feat: updated routes to use a middleware

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,18 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\ProjectController;
 use App\Http\Controllers\API\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
+Route::group(['middleware' => 'auth:sanctum'], function() {
+    Route::get('/profile', [AuthController::class, 'profile'])->name('profile');
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::apiResource('projects', ProjectController::class);
-Route::apiResource('tasks', TaskController::class);
+    Route::apiResource('projects', ProjectController::class);
+    Route::apiResource('tasks', TaskController::class);
+});
