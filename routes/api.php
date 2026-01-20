b6b0d0d90ce145372155a1992e8e39b8833dd183
Cristiano Abilio <cristianocafr@gmail.com>
2026-01-20 11:42:00 -0300

    feat: implemented rate limiting

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::group(['middleware' => 'auth:sanctum'], function() {
+Route::group(['middleware' => 'auth:sanctum', 'throttle:api'], function() {
     Route::get('/profile', [AuthController::class, 'profile'])->name('profile');
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
